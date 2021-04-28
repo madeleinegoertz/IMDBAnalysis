ec10d5bd38b67610f11847dccc156aa64f12805b
Madeleine Goertz <>
2021-04-28 23:42:06 +0000

    Finished exploring pandas functionality.

--- main.py
+++ main.py
@@ -15,3 +15,21 @@ print(df[cols])
 rating = df[df['imdb_score'] > 7.5]
 print(rating[['movie_title','imdb_score']])
 
+# How many movies came from each country?
+country = df['country'].value_counts()
+print(country.head(10))
+
+# Which director made the most money?
+df_gross = df.groupby('director_name')['gross'].sum()
+df_gross = df_gross.sort_values(ascending=False)
+print(df_gross.head(10))
+
+# df_gross['num_movies'] = df.groupby(['director_name'])['movie_title'].count().squeeze()
+# print(df_gross.head(10))
+
+# How many movies were made each year?
+df_movies_year = df.groupby(['title_year'])['movie_title'].count()
+print(df_movies_year.head(50))
+
+langs = df['language'].unique()
+print(langs)
